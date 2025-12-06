4774e03c285b9ed2131bc5696bfda73222545dcc
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-12-06 21:22:33 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,5 +8,5 @@ let package = Package(
                     .executable(name: "generator", targets: ["generator"])],
     dependencies:  [.package(url: "https://github.com/kelvin13/swiftxml", exact: "1.0.0")],
     targets:       [.target(name: "GL"        , dependencies: []      , path: "sources/opengl"),
-                    .target(name: "generator" , dependencies: ["XML"] , path: "sources/generator")]
+                    .target(name: "generator" , dependencies: ["swiftxml"] , path: "sources/generator")]
 )
